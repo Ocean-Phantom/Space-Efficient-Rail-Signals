51a9b29e67f15ca8c12c9b69b09f66956129a782
Ocean-Phantom <119775902+Ocean-Phantom@users.noreply.github.com>
2023-01-29 14:31:56 +0800

    1.0.3 release
    removed some logging functions for less clutter in log file
    
    foxed crash cause by nil values

--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,4 @@
-local cmu = require("collision-mask-util")
+local cmu = require("__core__.lualib.collision-mask-util")
 local colliding_signal_layers = {} --set of rail signal layers that collide with rail-segments
 local copy_of_rails = {}
 
@@ -52,50 +52,54 @@ local function edit_non_rail_segment()
       if prototype.type ~= "straight-rail" and prototype.type ~= "curved-rail" then
         if cmu.mask_contains_layer(prototype_mask, old_layer) then
           cmu.add_layer(prototype_mask, new_layer)
-          log("Adding "..tostring(new_layer).." collision mask for "..tostring(prototype.name))
+          -- log("Adding "..tostring(new_layer).." collision mask for "..tostring(prototype.name))
           -- log("collision masks: "..serpent.block(prototype.collision_mask))
           
           if prototype.type == "rail-signal" or prototype.type == "rail-chain-signal" then
             cmu.remove_layer(prototype_mask, old_layer)
-            log("Removing "..tostring(old_layer).." collision mask for "..tostring(prototype.name))
+            -- log("Removing "..tostring(old_layer).." collision mask for "..tostring(prototype.name))
             -- log("name: "..tostring(prototype.name).."\n type: "..tostring(prototype.type))
             -- log("collision masks: "..serpent.block(prototype.collision_mask))
           end
         end
-      -- else
-      --   if cmu.mask_contains_layer(prototype_mask,new_layer) then
-      --     cmu.remove_layer(prototype_mask, new_layer) --in case a mod tries to add this layer for some reason
-      --     log("Removing "..tostring(new_layer).." collision mask for "..tostring(prototype.name))
-      --   end
-      --   log("name: "..tostring(prototype.name).."\n type: "..tostring(prototype.type))
-      --   log("collision masks: "..serpent.block(prototype.collision_mask))
       end
     end
   end
 end
 
-local function all_rails_collide()
-  local t = true
-  for _, rail1 in pairs(data.raw["straight-rail"]) do
-    for _, rail2 in pairs(data.raw["straight-rail"]) do
-      if not cmu.masks_collide(rail1.collision_mask, rail2.collision_mask) then
-        log(tostring(rail1.name).."did not collide with "..tostring(rail2.name))
-        log("rail 1"..serpent.block(rail1.collision_mask))
-        log("rail 2"..serpent.block(rail2.collision_mask))
-        t = false
+local function prototypes_collide(prototype)
+  for _, prototype1 in pairs(data.raw[prototype]) do
+    
+    for _, prototype2 in pairs(data.raw[prototype]) do
+      prototype1.collision_mask = cmu.get_mask(prototype1)
+      prototype2.collision_mask = cmu.get_mask(prototype2)
+      if not cmu.masks_collide(prototype1.collision_mask, prototype2.collision_mask) then
+        log(tostring(prototype1.name).." did not collide with "..tostring(prototype2.name))
+        log("prototype 1"..serpent.block(prototype1.collision_mask))
+        log("prototype 2"..serpent.block(prototype2.collision_mask))
+        return false
       end
     end
   end
-  if t == true then
-    log("All rails collide")
-  else
-    log("Error: Rails are not collidiing. Please report this to the mod author")
+  return true
+end
+
+local function all_prototypes_collide()
+  local t = true
+  for _, prototype_type in pairs({"straight-rail", "curved-rail", "rail-signal", "rail-chain-signal"}) do
+    t = prototypes_collide(prototype_type)
+
+    if t == true then
+      log("All "..tostring(prototype_type).." collide")
+    else
+      log("Error: "..tostring(prototype_type).." are not collidiing. Please report this to the mod author")
+    end
   end
 end
 
 get_all_collisions()
--- log("Layers to replace:"..serpent.block(colliding_signal_layers))
+log("Layers to replace:"..serpent.block(colliding_signal_layers))
 copy_all_rails(data.raw, copy_of_rails)
 edit_non_rail_segment()
 copy_all_rails(copy_of_rails, data.raw)
--- all_rails_collide()
+all_prototypes_collide()
